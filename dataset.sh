2091025eb44fb4ce413b0ab483232c0a5bc61fdd
Starry03 <andrea.starrantino@gmail.com>
2025-09-29 20:08:16 +0200

    Add dataset generation script for map planning

---
+++ dataset.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+MAP_FOLDER="./maps/"
+SOLUTIONS_FOLDER="./solutions/"
+TRAINSET_FOLDER="./trainset/"
+
+MAP_SIZE=20
+NUM_AGENTS=10
+NUM_DATASET=30000
+MAP_COMPLEXITY=0.005
+MAP_DENSITY=0.1
+
+python Generate_cropmap.py  --random_map --gen_CasePool --gen_map_type \
+    random --chosen_solver ECBS --map_width ${MAP_SIZE} --map_density ${MAP_DENSITY}  \
+    --map_complexity ${MAP_COMPLEXITY} --num_agents ${NUM_AGENTS} --num_dataset ${NUM_DATASET} \
+    --num_caseSetup_pEnv 50 --path_save ${MAP_FOLDER}
+
+python CasesSolver_cropfromMap_fixedLength.py --loadmap_TYPE \
+    random --random_map --gen_CasePool --chosen_solver ECBS --map_width ${MAP_SIZE} \
+    --map_density ${MAP_DENSITY}  --map_complexity ${MAP_COMPLEXITY} --num_agents ${NUM_AGENTS} \
+    --num_dataset ${NUM_DATASET} --num_caseSetup_pEnv 50 \
+    --path_save ${SOLUTIONS_FOLDER} --path_loadSourceMap ${MAP_FOLDER}
+
+python DataGen_Transformer_split_IDMap.py  --num_agents ${NUM_AGENTS} --map_w ${MAP_SIZE} \
+    --map_density ${MAP_DENSITY}   --div_train 21000 --div_valid 4500 --div_test 4500 \
+    --div_train_IDMap 0 --div_test_IDMap 427 --div_valid_IDMap 800 \
+    --solCases_dir ${SOLUTIONS_FOLDER}   --dir_SaveData ${TRAINSET_FOLDER}  \
+    --guidance Project_G
